cd98a547a2fa54bc56a059628ce021fb9aa5c1d0
Loriella <loris.orlova@gmail.com>
2019-07-02 14:45:25 +0300

    parameter transfer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 class App extends Component {
   render () {
@@ -9,6 +10,10 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>Hello</h1>
+
+        <Car name={'Ford'} year={2018}/>
+        <Car name={'Audi'} year={2016}/>
+        <Car name={'Mazda'} year={2010}/>
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-function Car() {
-  return (
-    <h2>This is car component</h2>
-  )
-}
-
-export default Car;
+export default props => (
+  <div>
+    <h3>Car name: {props.name}</h3>
+    <p>Year: <strong>{props.year}</strong></p>
+  </div>
+)
