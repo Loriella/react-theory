83cb8329608dcc59afc12ba6014ee693f9791922
Loriella <loris.orlova@gmail.com>
2019-07-02 14:51:34 +0300

    content transfer (props.children)

--- src/App.js
+++ src/App.js
@@ -11,9 +11,18 @@ class App extends Component {
       <div style={divStyle}>
         <h1>Hello</h1>
 
-        <Car name={'Ford'} year={2018}/>
-        <Car name={'Audi'} year={2016}/>
-        <Car name={'Mazda'} year={2010}/>
+        <Car name={'Ford'} year={2018}>
+          <p style={{color: 'blue'}}>Color</p>
+        </Car>
+
+        <Car name={'Audi'} year={2016}>
+          <p style={{color: 'red'}}>Color</p>
+        </Car>
+
+        <Car name={'Mazda'} year={2010}>
+          <p style={{color: 'green'}}>Color</p>
+        </Car>
+
       </div>
     );
   }
