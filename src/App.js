88d32f3026aea666afdd4929e20049e00e9363ea
Loriella <loris.orlova@gmail.com>
2019-07-03 17:38:32 +0300

    input field processing

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ class App extends Component {
     })
   };
 
+  handleInput = (event) => {
+    this.setState({
+      pageTitle: event.target.value
+    })
+  };
+
   render () {
     const divStyle = {
       textAlign: 'center'
@@ -30,6 +36,8 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+
+        <input type="text" onChange={this.handleInput}/>
 
         <button
           onClick={this.changeTitleHandler.bind(this, 'Changed!')}
